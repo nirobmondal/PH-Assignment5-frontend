176e89210dbe2b5191ed75fa7fcbdf399a273b54
nirobmondal <nirobmondal202@gmail.com>
2026-04-28 19:52:40 +0600

    reusable table added

---
+++ src/components/shared/cell/StatusBadgeCell.tsx
@@ -0,0 +1,29 @@
+import { Badge } from "@/components/ui/badge";
+
+export enum UserStatus {
+  ACTIVE = "ACTIVE",
+  BANNED = "BANNED",
+}
+
+interface IStatusBadgeCellProps {
+  status: UserStatus;
+}
+
+const StatusBadgeCell = ({ status }: IStatusBadgeCellProps) => {
+  return (
+    <Badge
+      variant={
+        status === UserStatus.ACTIVE
+          ? "default"
+          : status === UserStatus.BANNED
+            ? "destructive"
+            : "secondary"
+      }
+      // className="px-2 py-1"
+    >
+      <span className="text-sm capitalize">{status.toLowerCase()}</span>
+    </Badge>
+  );
+};
+
+export default StatusBadgeCell;

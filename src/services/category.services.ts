4f32a3efd245da06b441722cf26e033cb49425bc
nirobmondal <nirobmondal202@gmail.com>
2026-05-12 21:30:56 +0600

    category and manufacturer service layer added

--- src/services/category.services.ts
+++ src/services/category.services.ts
@@ -1,3 +1,83 @@
-/**
- * 1. category crud. admin can create, update, delete and get all categories. user can only get all categories.
- */
+"use server";
+
+import { httpClient } from "@/lib/axios/httpClient";
+import { ApiErrorResponse, ApiResponse } from "@/types/api.types";
+import { ICategoryResponse } from "@/types/category.types";
+import {
+  createCategoryZodSchema,
+  ICreateCategoryPayload,
+} from "@/zod/category.validation";
+
+export const createCategory = async (
+  payload: ICreateCategoryPayload,
+): Promise<ApiResponse<unknown>> => {
+  const parsedPayload = createCategoryZodSchema.safeParse(payload);
+
+  if (!parsedPayload.success) {
+    return {
+      success: false,
+      message: parsedPayload.error.issues[0].message || "Invalid input",
+    };
+  }
+
+  try {
+    return await httpClient.post<unknown>("/category", parsedPayload.data);
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const getCategories = async (): Promise<
+  ApiResponse<ICategoryResponse[] | ApiErrorResponse>
+> => {
+  try {
+    return await httpClient.get<ICategoryResponse[]>("/category");
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const updateCategory = async (
+  categoryId: string,
+  payload: ICreateCategoryPayload,
+): Promise<ApiResponse<unknown>> => {
+  const parsedPayload = createCategoryZodSchema.safeParse(payload);
+
+  if (!parsedPayload.success) {
+    return {
+      success: false,
+      message: parsedPayload.error.issues[0].message || "Invalid input",
+    };
+  }
+
+  try {
+    return await httpClient.patch<unknown>(
+      `/category/${categoryId}`,
+      parsedPayload.data,
+    );
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const deleteCategory = async (
+  categoryId: string,
+): Promise<ApiResponse<unknown>> => {
+  try {
+    return await httpClient.delete<unknown>(`/category/${categoryId}`);
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
--- src/services/manufacturer.services.ts
+++ src/services/manufacturer.services.ts
@@ -1,3 +1,85 @@
-/**
- * 1. manufacturer crud. admin can create, update, delete and get all manufacturers. user can only get all manufacturers.
- */
+"use server";
+
+import { httpClient } from "@/lib/axios/httpClient";
+import { ApiErrorResponse, ApiResponse } from "@/types/api.types";
+import { IManufacturerResponse } from "@/types/manufacturer.types";
+import {
+  createManufacturerZodSchema,
+  ICreateManufacturerPayload,
+  IUpdateManufacturerPayload,
+  updateManufacturerZodSchema,
+} from "@/zod/manufacturer.validation";
+
+export const createManufacturer = async (
+  payload: ICreateManufacturerPayload,
+): Promise<ApiResponse<unknown>> => {
+  const parsedPayload = createManufacturerZodSchema.safeParse(payload);
+
+  if (!parsedPayload.success) {
+    return {
+      success: false,
+      message: parsedPayload.error.issues[0].message || "Invalid input",
+    };
+  }
+
+  try {
+    return await httpClient.post<unknown>("/manufacturer", parsedPayload.data);
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const getManufacturers = async (): Promise<
+  ApiResponse<IManufacturerResponse[] | ApiErrorResponse>
+> => {
+  try {
+    return await httpClient.get<IManufacturerResponse[]>("/manufacturer");
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const updateManufacturer = async (
+  manufacturerId: string,
+  payload: IUpdateManufacturerPayload,
+): Promise<ApiResponse<unknown>> => {
+  const parsedPayload = updateManufacturerZodSchema.safeParse(payload);
+
+  if (!parsedPayload.success) {
+    return {
+      success: false,
+      message: parsedPayload.error.issues[0].message || "Invalid input",
+    };
+  }
+
+  try {
+    return await httpClient.patch<unknown>(
+      `/manufacturer/${manufacturerId}`,
+      parsedPayload.data,
+    );
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const deleteManufacturer = async (
+  manufacturerId: string,
+): Promise<ApiResponse<unknown>> => {
+  try {
+    return await httpClient.delete<unknown>(`/manufacturer/${manufacturerId}`);
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};

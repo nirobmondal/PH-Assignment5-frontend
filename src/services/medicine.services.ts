7dca75c704506450589cc8015f238943f5d756e8
nirobmondal <nirobmondal202@gmail.com>
2026-05-12 21:32:28 +0600

    medicine and seller profile create service layyer added

---
+++ src/services/medicine.services.ts
@@ -0,0 +1,102 @@
+"use server";
+
+import { httpClient } from "@/lib/axios/httpClient";
+import { ApiErrorResponse, ApiResponse } from "@/types/api.types";
+import {
+  IMedicineListResponse,
+  IMedicineSellerListResponse,
+  MedicineWithRelations,
+} from "@/types/medicine.types";
+
+export const createMedicine = async (
+  payload: FormData,
+): Promise<ApiResponse<unknown>> => {
+  try {
+    return await httpClient.post<unknown>("/medicine", payload, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const getAllMedicines = async (
+  queryString: string,
+): Promise<ApiResponse<IMedicineListResponse | ApiErrorResponse>> => {
+  try {
+    return await httpClient.get<IMedicineListResponse>(
+      queryString ? `/medicine?${queryString}` : "/medicine",
+    );
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const getMedicineById = async (
+  medicineId: string,
+): Promise<ApiResponse<MedicineWithRelations | ApiErrorResponse>> => {
+  try {
+    return await httpClient.get<MedicineWithRelations>(
+      `/medicine/${medicineId}`,
+    );
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const getMedicinesBySeller = async (
+  queryString: string,
+): Promise<ApiResponse<IMedicineSellerListResponse | ApiErrorResponse>> => {
+  try {
+    return await httpClient.get<IMedicineSellerListResponse>(
+      queryString ? `/medicine/seller?${queryString}` : "/medicine/seller",
+    );
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const updateMedicine = async (
+  medicineId: string,
+  payload: FormData,
+): Promise<ApiResponse<unknown>> => {
+  try {
+    return await httpClient.patch<unknown>(`/medicine/${medicineId}`, payload, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
+
+export const deleteMedicine = async (
+  medicineId: string,
+): Promise<ApiResponse<unknown>> => {
+  try {
+    return await httpClient.delete<unknown>(`/medicine/${medicineId}`);
+  } catch (error: any) {
+    return {
+      success: false,
+      message: `Request failed: ${error.message}`,
+    };
+  }
+};
---
+++ src/services/seller.services.ts
@@ -0,0 +1,62 @@
+"use server";
+
+import { getDefaultDashboardRoute, UserRole } from "@/lib/authUtils";
+import { httpClient } from "@/lib/axios/httpClient";
+import { setTokenInCookies } from "@/lib/tokenUtils";
+import { ApiErrorResponse, ApiResponse } from "@/types/api.types";
+import { ISellerResponse } from "@/types/seller.types";
+
+import {
+  createSellerProfileSchema,
+  ICreateSellerProfilePayload,
+} from "@/zod/seller.validation";
+import { redirect } from "next/navigation";
+
+export const createSellerProfile = async (
+  payload: ICreateSellerProfilePayload,
+): Promise<ApiResponse<ISellerResponse | ApiErrorResponse>> => {
+  const parsedPayload = createSellerProfileSchema.safeParse(payload);
+
+  if (!parsedPayload.success) {
+    const firstError = parsedPayload.error.issues[0].message || "Invalid input";
+    return {
+      success: false,
+      message: firstError,
+    };
+  }
+  try {
+    const response = await httpClient.post<ISellerResponse>(
+      "/seller/create-profile",
+      parsedPayload.data,
+    );
+
+    if (!response.success) {
+      return response;
+    }
+
+    const { accessToken, refreshToken, token, user } = response.data;
+    const { role } = user;
+    await setTokenInCookies("accessToken", accessToken);
+    await setTokenInCookies("refreshToken", refreshToken);
+    await setTokenInCookies("better-auth.session_token", token, 24 * 60 * 60);
+
+    const targetPath = getDefaultDashboardRoute(role as UserRole);
+    redirect(targetPath);
+  } catch (error: any) {
+    console.log(error, "error");
+    if (
+      error &&
+      typeof error === "object" &&
+      "digest" in error &&
+      typeof error.digest === "string" &&
+      error.digest.startsWith("NEXT_REDIRECT")
+    ) {
+      throw error;
+    }
+
+    return {
+      success: false,
+      message: `Seller profile creation failed: ${error.message}`,
+    };
+  }
+};
